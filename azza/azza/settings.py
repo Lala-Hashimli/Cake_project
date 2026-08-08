c3eb1f7bd7273438d594a7e5c7c8c6222ceb8237
Lala-Hashimli <hshml.lala@gmail.com>
2026-08-08 12:34:30 +0400

    add swagger, uuid

--- azza/azza/settings.py
+++ azza/azza/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'cakes',
     "rest_framework",
+    "drf_yasg"
 ]
 
 MIDDLEWARE = [
--- azza/cakes/models.py
+++ azza/cakes/models.py
@@ -1,8 +1,13 @@
 from django.db import models
-
+import uuid
 # Create your models here.
 
 class Cake(models.Model):
+    id = models.UUIDField(
+        primary_key=True,
+        default=uuid.uuid4,
+        editable=False
+    )
     name = models.CharField(max_length=100)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     discount_price = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
