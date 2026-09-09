fc1ce24c566e79523e3402cdeb077c8f26885ea9
Lala-Hashimli <hshml.lala@gmail.com>
2026-09-09 19:21:16 +0400

    create/update time

--- azza/cakes/models.py
+++ azza/cakes/models.py
@@ -56,6 +56,9 @@ class Cake(models.Model):
         blank=True
     )
 
+    created_at = models.DateTimeField(auto_now_add=True)
+    update_at = models.DateTimeField(auto_now=True)
+    
     def __str__(self):
         return self.name
     
